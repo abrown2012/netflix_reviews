ff49304613e8b78b0a7285cb541aeb398e0dacce
Aga <abrown2012@icloud.com>
2021-11-20 18:20:50 -0500

    adds netflix show component

---
+++ app/javascript/components/NetflixShow/NetflixShow.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NetflixShow = () => {
+    return (
+        <div>
+            This is the NetflixShow#show view for my app
+        </div>
+    )
+}
+
+export default NetflixShow
--- app/javascript/components/NetflixShows/NetflixShows.js
+++ app/javascript/components/NetflixShows/NetflixShows.js
@@ -7,3 +7,5 @@ const NetflixShows = () => {
         </div>
     )
 }
+
+export default NetflixShows 
